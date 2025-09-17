d75e88a919c1ca037e2543e5a4b86788fa60e761
StephanieMildner <mildners@student.elms.edu>
2025-09-17 11:49:50 -0400

    currency converter using UI and buttons

--- Lab 3 Currency Converter UI/script.js
+++ Lab 3 Currency Converter UI/script.js
@@ -1,8 +1,16 @@
 
-function hello(){
-name = document.getElementById("username").value;
-    alert("Hello " + name + "!");
+//function hello(){
+//name = document.getElementById("username").value;
+ //   alert("Hello " + name + "!");
+//}
+//function bye(){
+ //   alert("Goodbye!");
+//} 
+function convert(){
+    money = document.getElementById("money").value;
+    let num = money * 9.32;
+    let n = num.toFixed(2);
+    document.getElementById("krona").value = n ;
+    
+
 }
-function bye(){
-    alert("Goodbye!");
-} 
