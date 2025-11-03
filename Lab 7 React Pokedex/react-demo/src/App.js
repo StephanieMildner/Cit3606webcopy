e8926e4637b30fea2475427858e70397263e543b
StephanieMildner <mildners@student.elms.edu>
2025-11-03 12:05:19 -0500

    pokemon api labn 7

--- Lab 7 React Pokedex/react-demo/src/App.js
+++ Lab 7 React Pokedex/react-demo/src/App.js
@@ -2,20 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 
 
+
 import {useState} from "react"
 
-function Pokemon(props){
-    return(
-        <div>
-            <h4>
-              <br></br>
-              <br></br>
-              
 
-            
-            </h4>
-            <div class="card">
-  <img src= {props.image}/>
+
+function Search(){
+  const[name, setName]=useState("Eevee");
+  const[type, setType]=useState("Normal");
+  const[image, setImage]=useState("eevee.png");
+  function changeState(e) {    setName(e.target.value);  } // asgn value
+  function clickHandler() {   
+ fetch("https://pokeapi.co/api/v2/pokemon/" + name)
+    .then(response => response.json())
+    .then(data => {  
+      setType(data.types[0].type.name);
+      setImage(data.sprites.front_default);
+  });
+        
+  }
+
+  return(
+    <div>
+    <label>Choose a Pokemon! 
+    <input type="text" onChange={changeState} />
+     <button onClick={clickHandler}>Enter </button>
+    </label>
+  <Pokemon name={name} type={type} image={image}/>
+    </div>
+  );
+}
+
+
+function Pokemon(props){          
+  return(        
+  <div>
+  <h4>
+  Pokedex Cards
+  </h4>
+  <div class="card">
+  <img src= {props.image} width="80%"/>
   <div class="container">
     <h4><b>{props.name}</b></h4>
     <p>Type = {props.type}</p>
@@ -24,11 +50,11 @@ function Pokemon(props){
             </div>
     )}
          
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Search/>
       <Pokemon name="Eevee" type="Normal" image="eevee.png"/>
         <Pokemon name="Umbreon" type="Dark" image="umbreon.png"/>
         <Pokemon name="Vaporeon" type="Water" image="vaporeon.png"/>
@@ -36,6 +62,9 @@ function App() {
        
        
       </header>
+
+
+
     </div>
   );
 }
